03bdd279a215494b0a05a5d5eb2897f49ff649be
ishakzail <ishak.zail.iz@gmail.com>
2022-04-08 14:28:25 +0000

    add a function to check walls in the map

--- mandatory/checks.c
+++ mandatory/checks.c
@@ -19,11 +19,55 @@ int	check_rectangular(t_game *game)
 	lines = 1;
 	while (game->map[lines])
 	{
-        // ft_printf("lines = %s\n", game->map[lines]);
-        // ft_printf("strlen = %d\n", ft_strlen(game->map[0]));
 		if (ft_strlen(game->map[0]) != ft_strlen(game->map[lines]))
 			return (0);
 		lines++;
+	}
+	return (1);
+}
+
+int check_line(t_game *game)
+{
+	int	i;
+	int	j;
+	int len;
+
+	i = -1;
+	len = ft_strlen(game->map[i+1]);
+	while (game->map[++i])
+	{
+		j = -1;
+		while (++j < len)
+			if (game->map[i][j] != '0' && game->map[i][j] != '1' && game->map[i][j] != 'E'
+			&& game->map[i][j] != 'C' && game->map[i][j] != 'P')
+			return (0);
+	}
+	return (1);
+}
+
+int	check_walls(t_game *game)
+{
+	int	i;
+	int	j;
+	int	len;
+
+	j = 0;
+	i = 0;
+	while (game->map[i] != '\0')
+		i++;
+	while (game->map[0][j] != '\0' && game->map[i - 1][j] != '\0')
+	{
+		if (game->map[0][j] != '1' || game->map[i - 1][j] != '1')
+			return (0);
+		j++;
+	}
+	i = 0;
+	len = ft_strlen(game->map[i]) - 1;
+	while (game->map[i] != '\0')
+	{
+		if (game->map[i][0] != '1' || game->map[i][len] != '1')
+			return (0);
+		i++;
 	}
 	return (1);
 }
