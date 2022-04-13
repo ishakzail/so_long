7426e2b1b9a7ee9bce1385e4c01d4e721823b17d
ishakzail <ishak.zail.iz@gmail.com>
2022-04-13 15:43:18 +0000

    map_init

--- mandatory/fill_map.c
+++ mandatory/fill_map.c
@@ -67,3 +67,14 @@ char	**fill_map(char *line, t_game *game)
 	game->map[++k] = 0;
 	return (game->map);
 }
+
+void	map_init(t_game *game)
+{
+	game->n_player = 0;
+	game->n_collect = check_obj(game, 'C');
+	game->n_exit = 0;
+	game->map_h = 0;
+	game->map_w = 0;
+	game->p_x = 0;
+	game->p_y = 0;
+}
