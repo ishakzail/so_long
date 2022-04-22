eb5556b6a8f884204a1f061ad0e2a8c52119dab6
ishakzail <ishak.zail.iz@gmail.com>
2022-04-22 14:37:13 +0000

    first commit in bonus part

---
+++ bonus/checks.c
@@ -0,0 +1,142 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checks.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: izail <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/22 14:14:51 by izail             #+#    #+#             */
+/*   Updated: 2022/04/22 14:14:52 by izail            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long_bonus.h"
+
+int	check_rectangular(t_game *game)
+{
+	int	lines;
+
+	lines = 1;
+	while (game->map[lines])
+	{
+		// ft_printf("len line == %d\n",ft_strlen(game->map[lines]));
+		if (ft_strlen(game->map[0]) != ft_strlen(game->map[lines]))
+			return (0);
+		lines++;
+	}
+	return (1);
+}
+
+int check_line(t_game *game)
+{
+	int	i;
+	int	j;
+	int len;
+
+	i = -1;
+	len = ft_strlen(game->map[i+1]);
+	while (game->map[++i])
+	{
+		j = -1;
+		while (++j < len)
+			if (game->map[i][j] != 48 && game->map[i][j] != 49 && game->map[i][j] != 69
+			&& game->map[i][j] != 67 && game->map[i][j] != 80)
+			return (0);
+	}
+	return (1);
+}
+
+int	check_walls(t_game *game)
+{
+	int	i;
+	int	j;
+	int	len;
+
+	j = 0;
+	i = 0;
+	while (game->map[i] != '\0')
+		i++;
+	while (game->map[0][j] != '\0' && game->map[i - 1][j] != '\0')
+	{
+		if (game->map[0][j] != '1' || game->map[i - 1][j] != '1')
+			return (0);
+		j++;
+	}
+	i = 0;
+	len = ft_strlen(game->map[i]) - 1;
+	while (game->map[i] != '\0')
+	{
+		if (game->map[i][0] != '1' || game->map[i][len] != '1')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	check_obj(t_game *game, char c)
+{
+	int	i;
+	int	j;
+	int len;
+	int obj;
+
+	obj = 0;
+	i = 1;
+	len = ft_strlen(game->map[i]) - 1;
+	while (game->map[i])
+	{
+		j = 1;
+		while (j < len)
+		{
+			if (game->map[i][j] == c)
+				obj++;
+			j++;
+		}
+		i++;
+	}
+	return (obj);
+}
+
+int	check_map_objects(t_game *game)
+{
+
+	if (!check_obj(game, 'P') && !check_obj(game, 'E') && !check_obj(game, 'C'))
+		return (ft_printf("All objects are missing"), 0);
+	else if ( !check_obj(game, 'E') && !check_obj(game, 'P'))
+		return (ft_printf("The exit and the player are missing !"), 0);
+	else if ( !check_obj(game, 'E') && !check_obj(game, 'C'))
+		return (ft_printf("The exit and the collectible are missing !"), 0);
+	else if ( !check_obj(game, 'P') && !check_obj(game, 'C'))
+		return (ft_printf("The player and the collectible are missing !"), 0);
+	else if (check_obj(game, 'P') != 1)
+		return (ft_printf("The map must contains only one player !"), 0);
+	else if (check_obj(game, 'E') != 1)
+		return (ft_printf("The map must contains only one exit !"), 0);
+	else if (check_obj(game, 'C') < 1)
+		return (ft_printf("The map must contains at least one collectible !"), 0);
+	return (1);
+}
+
+int	check_0(t_game *game)
+{
+	int	i;
+	int	j;
+	int count;
+
+	i = 1;
+	count = 0;
+	while (game->map[i])
+	{
+		j = 0;
+		while (game->map[i][j])
+		{
+			if (game->map[i][j] == '0')
+				count++;
+			j++;
+		}
+		i++;
+	}
+	if (count == 0)
+		return (ft_printf("Map is invalid !"), 0);
+	return (1);
+}
---
+++ bonus/so_long_bonus.h
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long_bonus.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: izail <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/22 14:21:12 by izail             #+#    #+#             */
+/*   Updated: 2022/04/22 14:22:36 by izail            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_BONUS_H
+# define SO_LONG_BONUS_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include "../includes/gnl/get_next_line.h"
+# include "../includes/ft_printf/ft_printf.h"
+# include <mlx.h>
+# include <math.h>
+
+# define A 0
+# define S 1
+# define D 2
+# define W 13
+# define ESC 53
+
+typedef struct s_game
+{
+	void	*mlx;
+	void	*win;
+	char	**map;
+	void	*img_backg;
+	void	*img_wall;
+	void	*img_player;
+	void	*img_collect;
+	void	*img_exit;
+    void    *img_enemies;
+	int		map_w;
+	int		map_h;
+	int		img_w;
+	int		img_h;
+	int		n_collect;
+	int		n_player;
+	int		n_exit;
+    int     pos_enemies;
+	int		p_x;
+	int		p_y;
+    int     e_x;
+    int     e_y;
+	int		moves;
+}	t_game;
+
+char    **ft_read_map(t_game *game, char *path);
+int		map_correct_form(t_game *game);
+char	*check(char *line, char *holder);
+void	ft_free(char *line, char *holder);
+char	**fill_map(char *line, t_game *game);
+char	**map_alloc(t_game *game, char *line);
+char	*word_dup(char *str, int start, int finish);
+
+void	free_map(t_game *game);
+void	map_init(t_game *game);
+
+/**************** Checks ****************/
+
+int	check_rectangular(t_game *game);
+int check_line(t_game *game);
+int	check_walls(t_game *game);
+int	check_obj(t_game *game, char c);
+int	check_map_objects(t_game *game);
+int	check_0(t_game *game);
+
+/**************** Draw ****************/
+
+void	display_map(t_game *game);
+void	draw(t_game *game);
+void	init_textures(t_game *game);
+void	image_drawer(t_game *game);
+void	drawer(t_game *game, int i, int j);
+
+/**************** Move Player ****************/
+
+void    player_position(t_game *game);
+
+int		destroy_window(t_game *game);
+void	move_player(t_game *game, int i, int j);
+int		m_hook(int key_code, t_game *game);
+void	move_a(t_game *game);
+void	move_d(t_game *game);
+void	move_w(t_game *game);
+void	move_s(t_game *game);
+
+void	win(t_game *game);
+
+#endif
